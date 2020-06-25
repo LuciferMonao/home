c2a73cf676af45089c9b033c371af23fc92256b7
Lucifer <lucifermonao@gmx.de>
2020-06-25 12:43:23 +0200

    Base form

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<?php
+    require "header.php";
+?>
+
+<main>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="styles/index-style.css">
+    </head>
+</main>
+
+<?php
+    require "footer.php";
+?>
+
+
+
+
